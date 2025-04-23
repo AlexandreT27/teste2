412df9e9e1953235f9f2ae582c46ba68a4076082
discente <discente@uirapuru.br>
2025-04-22 22:02:46 -0300

    desisao

---
+++ Aula de decisão/js/Parimpar.js
@@ -0,0 +1,9 @@
+var numero = parseInt(prompt("Digite um número: "))
+
+
+    if(numero %2 == 0){
+        console.log("Par");
+
+    }else{
+        console.log("Ímpar")
+    }
---
+++ Aula de decisão/js/media.js
@@ -0,0 +1,15 @@
+var nota1 = 1
+var nota2 = 1
+var nota3 = 1
+
+var media = (nota1 + nota2 + nota3)
+
+
+if(media < 5){
+    console.log("Reprovado")
+}else if((media >= 5) && (media < 7)){
+    console.log("Recuperação")
+}else{
+    console.log("Aprovado")
+    
+}
